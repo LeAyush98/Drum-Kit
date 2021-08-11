55546706d928a27db0baa8711911b3b78b332292
Ayush Sharma <88797898+LeAyush98@users.noreply.github.com>
2021-08-12 00:35:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var sound;
+for (var i=0 ; i<document.querySelectorAll("button").length; i++)
+{document.querySelectorAll("button")[i].addEventListener("click",function () {
+    var thisInnerHTML = this.innerHTML;
+   makeSound(thisInnerHTML);
+   buttonAnimation(thisInnerHTML);
+} );
+}
+
+document.addEventListener("keydown" , function (event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    }
+);
+
+function makeSound(key){
+    switch(key){
+        case "w": var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+        case "a": var kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+
+        case "s": var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+        case "d": var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+        case "j": var  tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+        case "k": var  tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+        case "l": var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        default: console.log("e");
+    }
+
+}
+
+function buttonAnimation(keyPress){
+ 
+    var animate = document.querySelector("." + keyPress);
+    animate.classList.toggle("pressed");
+   
+
+}
